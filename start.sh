c3ff9b76abe7dbcfb1a6c4e81af588d7e04310fa
scotfu <scotfu@gmail.com>
2014-12-04 22:43:45 +0000

    changes on start script

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
-uwsgi -s uwsgi.sock --module wsgi --callable app --master --processes 4 --threads 2 --stats 127.0.0.1:5001
+uwsgi --socket 127.0.0.1:5000 --wsgi-file wsgi.py --callable app --master --processes 4 --threads 2 --stats 127.0.0.1:5001
+
+
 
